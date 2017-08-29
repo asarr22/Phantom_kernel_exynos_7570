77056bb725879242824dc1d87d655e852ace4d2c
engstk <eng.stk@sapo.pt>
2017-08-30 00:50:43 +0200

    cpufreq: blu_active Governor And enable it
    Signed-off-by: hendramanu <hendra32os@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -511,6 +511,9 @@ extern struct cpufreq_governor cpufreq_gov_sched;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
 extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intellidemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
+extern struct cpufreq_governor cpufreq_gov_blu_active;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
 #endif
 
 /*********************************************************************
