58bc2f9c0b592be57679e768c9d6a1d07bc3a679
hendramanu <hendra32os@gmail.com>
2020-10-16 08:07:02 +0700

    cpufreq: Intellidemand cpu governor Thanks to @djb77 @ananjaser1211  @OGSPARTANICUS

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -503,6 +503,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
+extern struct cpufreq_governor cpufreq_gov_intellidemand;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intellidemand)
 #endif
 
 /*********************************************************************
